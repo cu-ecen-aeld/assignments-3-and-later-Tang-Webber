e05e8a04bb23a42972e25839f215dff0375aec82
webber <webber@ubuntu.myguest.virtualbox.org>
2023-06-02 02:10:02 +0800

    hw3-2 complete

--- finder-app/finder.sh
+++ finder-app/finder.sh
@@ -1,41 +1,21 @@
-#!/bin/bash
+#!/bin/sh
 
-# Check if both arguments are provided
-if [ $# -ne 2 ]; then
-    echo "Usage: $0 <filesdir> <searchstr>"
-    exit 1
+if [ ! $# -eq 2 ]
+then
+	echo "parameters above were not specified"
+	exit 1
 fi
 
-# Extract the arguments
-filesdir="$1"
-searchstr="$2"
+filesdir=$1
+searchstr=$2
 
-# Check if filesdir is a directory
-if [ ! -d "$filesdir" ]; then
-    echo "Error: $filesdir is not a directory."
-    exit 1
+if [ ! -d $1 ]
+then
+	echo "filesdir does not represent a directory on the filesystem"
+	exit 1
 fi
 
-# Find files and count matching lines
-num_files=0
-num_matching_lines=0
+X=$(find $1 -type f | wc -l)
+Y=$(grep -r "$2" $1/* | wc -l)
 
-# Recursively search for matching lines in files
-search_files() {
-    local dir=$1
-    local search=$2
-
-    for file in "$dir"/*; do
-        if [ -f "$file" ]; then
-            ((num_files++))
-            match_count=$(grep -c "$search" "$file")
-            ((num_matching_lines+=match_count))
-        elif [ -d "$file" ]; then
-            search_files "$file" "$search"
-        fi
-    done
-}
-search_files "$filesdir" "$searchstr"
-
-# Print the results
-echo "The number of files are $num_files and the number of matching lines are $num_matching_lines"
+echo "The number of files are $X and the number of matching lines are $Y"
--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -20,7 +20,6 @@ else
 	OUTDIR=$1
 	echo "Using passed directory ${OUTDIR} for output"
 fi
-
 mkdir -p ${OUTDIR}
 
 cd "$OUTDIR"
@@ -29,15 +28,22 @@ if [ ! -d "${OUTDIR}/linux-stable" ]; then
 	echo "CLONING GIT LINUX STABLE VERSION ${KERNEL_VERSION} IN ${OUTDIR}"
 	git clone ${KERNEL_REPO} --depth 1 --single-branch --branch ${KERNEL_VERSION}
 fi
+
 if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
     cd linux-stable
     echo "Checking out version ${KERNEL_VERSION}"
     git checkout ${KERNEL_VERSION}
 
-    # TODO: Add your kernel build steps here
+    # Add your kernel build steps here
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} mrproper
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} defconfig
+    make -j4 ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} all
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} modules
+    make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} dtbs
 fi
 
 echo "Adding the Image in outdir"
+cp ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ${OUTDIR}
 
 echo "Creating the staging directory for the root filesystem"
 cd "$OUTDIR"
@@ -47,34 +53,72 @@ then
     sudo rm  -rf ${OUTDIR}/rootfs
 fi
 
-# TODO: Create necessary base directories
+# Create necessary base directories
+mkdir -p ${OUTDIR}/rootfs
+cd ${OUTDIR}/rootfs
+mkdir -p bin dev etc home lib lib64 proc sbin sys tmp usr var
+mkdir -p var/log
+mkdir -p usr/bin usr/lib usr/sbin 
 
 cd "$OUTDIR"
 if [ ! -d "${OUTDIR}/busybox" ]
 then
-git clone git://busybox.net/busybox.git
+    git clone git://busybox.net/busybox.git
     cd busybox
     git checkout ${BUSYBOX_VERSION}
-    # TODO:  Configure busybox
+
+    # Configure busybox
+    echo "Configure busybox" 
+    make distclean
+    make defconfig
+
 else
     cd busybox
 fi
 
-# TODO: Make and install busybox
+# Make and install busybox
+make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
+make CONFIG_PREFIX=${OUTDIR}/rootfs ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
 
 echo "Library dependencies"
+cd ${OUTDIR}/rootfs
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "program interpreter"
 ${CROSS_COMPILE}readelf -a bin/busybox | grep "Shared library"
 
-# TODO: Add library dependencies to rootfs
+# Add library dependencies to rootfs
+echo "Add library dependencies to rootfs"
+SYSROOT=$(${CROSS_COMPILE}gcc -print-sysroot)
+cp ${SYSROOT}/lib/ld-linux-aarch64.so.1 lib/
+cp ${SYSROOT}/lib64/libm.so.6 lib64/
+cp ${SYSROOT}/lib64/libresolv.so.2 lib64/
+cp ${SYSROOT}/lib64/libc.so.6 lib64/
 
-# TODO: Make device nodes
+# Make device nodes
+cd ${OUTDIR}/rootfs
+sudo mknod -m 666 dev/null c 1 3
+sudo mknod -m 600 dev/console c 5 1
 
-# TODO: Clean and build the writer utility
+# Clean and build the writer utility
+cd $FINDER_APP_DIR
+make
+make clean
+make CROSS_COMPILE=${CROSS_COMPILE} # cross compile the app 
 
-# TODO: Copy the finder related scripts and executables to the /home directory
+# Copy the finder related scripts and executables to the /home directory
 # on the target rootfs
+cp ${FINDER_APP_DIR}/writer $OUTDIR/rootfs/home/
+cp ${FINDER_APP_DIR}/writer.c $OUTDIR/rootfs/home
+cp ${FINDER_APP_DIR}/finder.sh $OUTDIR/rootfs/home/
+cp ${FINDER_APP_DIR}/finder-test.sh $OUTDIR/rootfs/home/
+cp ${FINDER_APP_DIR}/autorun-qemu.sh $OUTDIR/rootfs/home/
+cp -r ${FINDER_APP_DIR}/conf/ $OUTDIR/rootfs/
+cp -r ${FINDER_APP_DIR}/conf $OUTDIR/rootfs/home/
 
-# TODO: Chown the root directory
+# Chown the root directory
+cd $OUTDIR/rootfs
+sudo chown -R root:root *
 
-# TODO: Create initramfs.cpio.gz
+# Create initramfs.cpio.gz
+find . | cpio -H newc -ov --owner root:root > ${OUTDIR}/initramfs.cpio
+cd ..
+gzip -f initramfs.cpio
