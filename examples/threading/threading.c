f9883d52373554a5cf7a1bede656781bf2235057
webber <webber@ubuntu.myguest.virtualbox.org>
2023-06-02 22:04:12 +0800

    HW4-1 complete

--- examples/threading/threading.c
+++ examples/threading/threading.c
@@ -13,7 +13,42 @@ void* threadfunc(void* thread_param)
 
     // TODO: wait, obtain mutex, wait, release mutex as described by thread_data structure
     // hint: use a cast like the one below to obtain thread arguments from your parameter
-    //struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    struct thread_data* thread_func_args = (struct thread_data *) thread_param;
+    
+    //wait for obtain and check
+    int obtain_status = usleep(thread_func_args->wait_to_obtain_ms * 1000);
+    if (obtain_status != 0) {
+        ERROR_LOG("obtain_status error code: %d", obtain_status);
+        thread_func_args->thread_complete_success = false;
+        return thread_param;
+    } 
+    //obtain mutex and check
+    int mutex_lock_status = pthread_mutex_lock(thread_func_args->mutex);
+    if (mutex_lock_status != 0) {
+        ERROR_LOG("mutex_lock_status error code: %d", mutex_lock_status);
+        thread_func_args->thread_complete_success = false;
+        return thread_param;
+    }
+    //wait for release and check
+    int release_status = usleep(thread_func_args->wait_to_obtain_ms * 1000);
+    if (obtain_status != 0) {
+        ERROR_LOG("release_status error code: %d", release_status);
+        thread_func_args->thread_complete_success = false;
+        return thread_param;
+    }    
+    //obtain mutex and check
+    int mutex_unlock_status = pthread_mutex_unlock(thread_func_args->mutex);
+    // check mutex_unlock_status
+    if (mutex_unlock_status != 0) {
+        ERROR_LOG("mutex_unlock_status error code: %d", mutex_unlock_status);
+        thread_func_args->thread_complete_success = false;
+        return thread_param;
+    }   
+    
+    //debug
+    DEBUG_LOG("Done Threadfunc");
+    
+    thread_func_args->thread_complete_success = true;   
     return thread_param;
 }
 
@@ -28,6 +63,28 @@ bool start_thread_obtaining_mutex(pthread_t *thread, pthread_mutex_t *mutex,int 
      *
      * See implementation details in threading.h file comment block
      */
-    return false;
-}
+// allocate memory dynamically for the thread data
 
+    struct thread_data *my_thread_data = (struct thread_data*)malloc(sizeof(struct thread_data*));
+
+    // check the memory allocation 
+    if (my_thread_data == NULL) {
+        ERROR_LOG("Malloc my_thread_data error");
+        return false;
+    }
+
+    // set up the struct thread data
+    my_thread_data->wait_to_obtain_ms = wait_to_obtain_ms;
+    my_thread_data->wait_to_release_ms = wait_to_release_ms;
+    my_thread_data->mutex = mutex;
+
+    int pthread_create_status = pthread_create(thread, NULL, threadfunc, my_thread_data);
+
+    if (pthread_create_status == 0) {
+    	return true;  
+    } 
+    
+    ERROR_LOG("pthread_create_status error code: %d", pthread_create_status);
+    return false;    
+   
+}
